06fd51df41b1782139c2c5013c709808701042bd
benzdht3 <thiendoan99999@gmail.com>
2024-06-05 09:34:40 +0700

    Implement advanced login with remember me option

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save {self.email = email.downcase}
 
   validates :name, presence: true, length: {maximum: 50}
@@ -10,4 +12,31 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :password, presence: true, length: {minimum:6}
+
+  #Return the hash digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string,cost: cost)
+  end
+
+  #Return a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #Remember a user in the database for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  #Return true if the given token match the digest
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  #Forget a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
